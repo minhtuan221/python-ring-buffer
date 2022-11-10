9e3730ec5830c04b5294997dd5f074885620ad16
Minh Tuan <ntuan221@gmail.com>
2022-11-10 16:00:45 +0000

    add share list obj

--- ring_buffer/services/shared_list_object.py
+++ ring_buffer/services/shared_list_object.py
@@ -1,21 +1,25 @@
 from multiprocessing import shared_memory
-from multiprocessing.managers import SharedMemoryManager
 import typing as t
 import pickle
-import math
 import time
 
 
 class SharedListObject:
-    def __init__(self, element_size=255, name='shared_list_object', create=True, size: int=100) -> None:
+    def __init__(self,
+                 name: str,
+                 size: int,
+                 element_size=255,
+                 create=True) -> None:
         self._element_size = element_size
         self.name = name
         # size of the element in ShareableList = math.ceil((element_size+1)/8)*8)
         if create:
-            self.share_list = shared_memory.ShareableList([bytes(self._element_size) for _ in range(size)],
-                                                          name=self.name)
+            self.share_list = \
+                shared_memory.ShareableList([bytes(self._element_size) for _ in range(size)],
+                                            name=self.name)
         else:
-            self.share_list = shared_memory.ShareableList(name=self.name)
+            self.share_list = \
+                shared_memory.ShareableList(name=self.name)
 
     def set(self, _index: int, obj: t.Any):
         obj_bytes = pickle.dumps(obj)
@@ -23,14 +27,14 @@ class SharedListObject:
 
     def get(self, _index: int):
         obj_bytes = self.share_list[_index]
-        if not len(obj_bytes):
+        if len(obj_bytes) == 0:
             return None
         return pickle.loads(obj_bytes)
 
     def remove(self, _index: int = -1) -> t.Any:
         obj_bytes = self.share_list[_index]
         self.share_list[_index] = bytes(self._element_size)
-        if not len(obj_bytes):
+        if len(obj_bytes) == 0:
             return None
         return pickle.loads(obj_bytes)
 
@@ -38,8 +42,9 @@ class SharedListObject:
         self.share_list.shm.unlink()
 
 
-def maintain_share_list_object(name='test1'):
-    smm = SharedListObject(name='test1')
+
+def maintain_share_list_object():
+    smm = SharedListObject('test1', 255)
     smm.set(0, 'first element')
     while True:
         time.sleep(10)
@@ -47,7 +52,7 @@ def maintain_share_list_object(name='test1'):
 
 
 def main():
-    smm = SharedListObject()
+    smm = SharedListObject('test', 16)
     _next = ''
     for i in range(20):
         _next += f'{i}'
