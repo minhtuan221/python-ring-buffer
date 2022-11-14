9b15328dd131e0d3c547cb9a5ffc74b158622551
Minh Tuan Nguyen <ntuan221@gmail.com>
2022-11-14 23:33:07 +0700

    convert stack to double linked list

--- ring_buffer/services/shared_dict_obj.py
+++ ring_buffer/services/shared_dict_obj.py
@@ -3,13 +3,13 @@ import pickle
 from multiprocessing import shared_memory
 
 
-_MAX_NAME_LENGHT = shared_memory._SHM_SAFE_NAME_LENGTH
+_MAX_NAME_LENGTH = shared_memory._SHM_SAFE_NAME_LENGTH
 _BYTES_DELIMITER = b'|'
 
 
-def _padding_name(n: bytes, max_lenght=_MAX_NAME_LENGHT):
+def _padding_name(n: bytes, max_length=_MAX_NAME_LENGTH):
     cleaned_n = n.strip()
-    return b' ' * max(max_lenght - len(cleaned_n), 0) + cleaned_n
+    return cleaned_n + b' ' * max(max_length - len(cleaned_n), 0)
 
 
 class Node:
@@ -19,9 +19,9 @@ class Node:
     def __init__(self,
                  name: t.Optional[str] = None,
                  create: bool = False,
-                 key_size: int = 14):
-        self.size = _MAX_NAME_LENGHT * 2 + 2 + key_size
-        self.key_size = key_size
+                 data_size: int = 14):
+        self.size = _MAX_NAME_LENGTH * 2 + data_size
+        self._data_size = data_size
         self.create = create
         if create:
             self.pointer = shared_memory.SharedMemory(
@@ -29,11 +29,11 @@ class Node:
         else:
             self.pointer = shared_memory.SharedMemory(name)
 
-    def build(self, previous_node_name: bytes, key: bytes, next_node_name: bytes):
+    def build(self, previous_node_name: bytes, data: bytes, next_node_name: bytes):
         if self.create:
             # build the buffer
-            bs = _BYTES_DELIMITER.join([_padding_name(previous_node_name), _padding_name(key), _padding_name(
-                next_node_name)])
+            bs = _padding_name(previous_node_name) + _padding_name(data) + _padding_name(
+                next_node_name)
             self.pointer.buf[:] = bs
 
     def _unpack(self):
@@ -44,27 +44,31 @@ class Node:
         return self.pointer.name
 
     def set_previous_node_name(self, name: bytes):
-        self.pointer.buf[:_MAX_NAME_LENGHT] = _padding_name(name)
+        self.pointer.buf[:_MAX_NAME_LENGTH] = _padding_name(name)
 
     def previous_node_name(self) -> str:
-        p, _, _ = self._unpack()
+        p = bytes(self.pointer.buf)[:14]
         return p.decode().strip()
 
-    def key(self) -> str:
-        _, p, _ = self._unpack()
+    def value(self) -> str:
+        p = bytes(self.pointer.buf)[14:-14]
         return p.decode().strip()
+
+    def data(self) -> bytes:
+        p = bytes(self.pointer.buf)[14:-14]
+        return p
 
     def set_next_node_name(self, name: bytes):
-        self.pointer.buf[-_MAX_NAME_LENGHT:] = _padding_name(name)
+        self.pointer.buf[-_MAX_NAME_LENGTH:] = _padding_name(name)
 
     def next_node_name(self) -> str:
-        _, _, p = self._unpack()
+        p = bytes(self.pointer.buf)[-14:]
         return p.decode().strip()
 
     def to_dict(self) -> dict:
         _d = {k: v for k, v in self.__dict__.items() if not k.startswith(' ')}
         _d['previous_node'] = self.previous_node_name()
-        _d['key'] = self.key()
+        _d['data'] = self.data()
         _d['next_node'] = self.next_node_name()
         return _d
 
@@ -83,9 +87,9 @@ class ValueNode(Node):
         if value:
             create = True
             value_in_bytes = pickle.dumps(value)
-            key_in_bytes = _padding_name(key.encode(), max_lenght=32)
+            key_in_bytes = _padding_name(key.encode(), max_length=32)
             buf_size = len(key_in_bytes) + len(value_in_bytes)
-            super().__init__(name, create=create, key_size=buf_size)
+            super().__init__(name, create=create, data_size=buf_size)
             
         else:
             create = False
@@ -96,43 +100,45 @@ class SharedStackObject:
     def __init__(self, name: str, create: bool = False):
         self.name = name
         self.create = create
-        self.size = _MAX_NAME_LENGHT  # name of the first node in stack
+        self.size = _MAX_NAME_LENGTH * 2  # name of the first node in stack
         if create:
             self.pointer = shared_memory.SharedMemory(
                 name, size=self.size, create=True)
         else:
             self.pointer = shared_memory.SharedMemory(name)
-        self.pointer.buf[:] = _padding_name(b'')
+        self.pointer.buf[:] = _padding_name(b'') * 2
         self._node_cache_map: t.Dict[str, Node] = {}
 
+    def _get_last_node(self):
+        n = bytes(self.pointer.buf[-14:]).decode()
+        return self.get_node(n.strip())
+
     def _get_first_node(self):
-        n = bytes(self.pointer.buf).decode()
+        n = bytes(self.pointer.buf[:14]).decode()
         return self.get_node(n.strip())
 
     def _is_first_node(self) -> bool:
         return len(bytes(self.pointer.buf).strip()) == 0
 
-    def append_node(self, key: str) -> Node:
+    def append_node(self, data: bytes) -> Node:
         # create new node
         node = Node(create=True)
         # check if it is the first node
         if not self._is_first_node():
-            first_node = self._get_first_node()
-            node.build(_padding_name(b''), key.encode(),
-                       first_node.name().encode())
-            first_node.set_previous_node_name(node.name().encode())
+            last_node = self._get_last_node()
+            node.build(last_node.name().encode(),
+                       data,
+                       _padding_name(b''))
+            last_node.set_next_node_name(node.name().encode())
         else:
-            node.build(_padding_name(b''), key.encode(), _padding_name(b''))
-        self.pointer.buf[:] = _padding_name(node.name().encode())
+            node.build(_padding_name(b''),
+                       data,
+                       _padding_name(b''))
+            # the first element name is the same as last
+            self.pointer.buf[:14] = _padding_name(node.name().encode())
+        self.pointer.buf[-14:] = _padding_name(node.name().encode())
         self._node_cache_map[node.name()] = node
         return node
-
-    def remove_node_by_key(self, key: str):
-        _nodes = self.get_all_nodes()
-        for node in _nodes:
-            if node.key() == key:
-                break
-        return self.remove_node(node.name())
 
     def remove_node(self, node_name: str) -> Node:
         # find the node by shared memory name
@@ -143,13 +149,28 @@ class SharedStackObject:
         _previous_node = self.get_node(_node.previous_node_name())
         # get the next node
         _next_node = self.get_node(_node.next_node_name())
-        # conext previous node to next node
-        _previous_node.set_next_node_name(_node.next_node_name().encode())
-        _next_node.set_previous_node_name(_node.previous_node_name().encode())
+        # swap previous node to next node
+        if _previous_node:
+            _previous_node.set_next_node_name(_node.next_node_name().encode())
+        else:
+            # this is removing the first element in linkedlist
+            if not _next_node:
+                self.pointer.buf[:14] = _padding_name(b' ')
+            else:
+                self.pointer.buf[:14] = _padding_name(_next_node.name().encode())
+        if _next_node:
+            _next_node.set_previous_node_name(_node.previous_node_name().encode())
+        else:
+            # this is removing the last element in linkedlist
+            if not _previous_node:
+                self.pointer.buf[-14:] = _padding_name(b' ')
+            else:
+                self.pointer.buf[-14:] = _padding_name(_previous_node.name().encode())
         # free removed node name memory
         _node.pointer.unlink()
         # remove node_name in cache
         self._node_cache_map.pop(node_name)
+        return  _node
 
     def get_node(self, node_name: str) -> t.Optional[Node]:
         _node_name = node_name.strip()
@@ -206,13 +227,17 @@ class SharedDictObject:
 
 def test_stack_object():
     stack = SharedStackObject('test', create=True)
-    stack.append_node('element1')
-    stack.append_node('e_2')
-    stack.append_node('e_3')
+    stack.append_node(b'e_1')
+    stack.append_node(b'e_2')
+    stack.append_node(b'e_3')
+    stack.append_node(b'e_4')
+    stack.append_node(b'e_5')
+    print(stack._node_cache_map)
     nodes = stack.get_all_nodes()
     for n in nodes:
+        if n.value() == 'e_1':
+            stack.remove_node(n.name())
         print(n)
-    stack.remove_node_by_key('e_2')
     print('------ stack after remove the second element ------ ')
     nodes = stack.get_all_nodes()
     for n in nodes:
